42f287060e2beff3f9360f308a839bd143badfd8
Ксюша <124886145+KsseniyaTerehova@users.noreply.github.com>
2023-04-07 21:53:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,45 +1,78 @@
 // Copyright 2021 NNTU-CS
-#include <string>
 #include <map>
 #include "tstack.h"
+int getPriori(char operation) {
+  switch (operation) {
+    case ('('):
+      return 0;
+      break;
+    case (')'):
+      return 1;
+      break;
+    case ('+'):
+      return 2;
+      break;
+    case ('-'):
+      return 2;
+      break;
+    case ('*'):
+      return 3;
+      break;
+    case ('/'):
+      return 3;
+      break;
+    default:
+      return -1;
+      break;
+  }  
+}
+std::string spases(const std::string& str) {
+    if (2 >= str.length()) return str;
+    int num = 2 - str.length() % 2;
+    std::string rez(str, 0, num);
+    for (auto it = str.begin() + num; it != str.end();) {
+        rez += ' '; rez += *it++;;
+    }
+    return rez;
+}      
 std::string infx2pstfx(std::string inf) {
-  std::string r;
-    TStack<char, 100> stack11;
-    for (auto& o : inf) {
-        int p = getP(o);
-        if (p == -1) {
-            r += o;
+  std::string rezult;
+  TStack<char, 100> stack11;
+  for (auto& operation : inf) {
+        int priori = getPriori(operation);
+        if (priori == -1) {
+            rezult += operation;
         } else {
-            if (stack11.get() < p|| p == 0 || stack11.isEmpty()) {
-                stack11.push(o);
-            } else if (o == ')') {
-                char sum = stack11.get();
-                while (getP(sum) >= p) {
-                    r += sum;
+            if (stack11.get() < priori || priori == 0 || stack11.isEmpty()) {
+                stack11.push(operation);
+            } else if (operation == ')') {
+                char summa = stack11.get();
+                while (getPriori(summa) >= priori) {
+                    rezult += summa;
                     stack11.pop();
-                    sum = stack11.get();
+                    summa = stack11.get();
                 }
                 stack11.pop();
             } else {
-                char sum = stack11.get();
-                while (getP(sum) >= p) {
-                    r += sum;
+                char summa = stack11.get();
+                while (getPriori(summa) >= priori) {
+                    rezult += summa;
                     stack11.pop();
-                    sum = stack11.get();
+                    summa = stack11.get();
                 }
-                stack11.push(o);
+                stack11.push(operation);
             }
         }
     }
     while (!stack11.isEmpty()) {
-        r += stack11.get();
+        rezult += stack11.get();
         stack11.pop();
     }
-    r = spases(r);
-    return r;
+    rezult = spases(rezult);
+    return rezult;
 }
 
-int sht(const int& x, const int& y, const int& op) {
+int vashislenia(const int& x, const int& y, const int& op) {
     switch (op) {
     case'+': return x + y;
     case'-': return x - y;
@@ -50,34 +83,36 @@ int sht(const int& x, const int& y, const int& op) {
     }
     return 0;
 }
-return std::string("");
-}
 
-int eval(std::string pref) {
-  int eval(std::string post) {
+int eval(std::string post) {
     TStack<int, 100> stack12;
-    std::string n = "";
+    std::string number = "";
     for (int i = 0; i < post.size(); i++) {
-        if (getP(post[i]) == -1) {
+        if (getPriori(post[i]) == -1) {
             if (post[i] == ' ') {
                 continue;
             } else if (isdigit(post[i + 1])) {
-                n += post[i];
+                number += post[i];
                 continue;
             } else {
-                n += post[i];
-                stack12.push(atoi(n.c_str()));
-                n = "";
+                number += post[i];
+                stack12.push(atoi(number.c_str()));
+                number = "";
             }
         } else {
             int y = stack12.get();
             stack12.pop();
             int x = stack12.get();
             stack12.pop();
-            stack12.push(sht(x, y, post[i]));
+            stack12.push(vashislenia(x, y, post[i]));
         }
     }
     return stack12.get();
 }
-  return 0;
-}
+
+
+  
+      
+      
+      
+      
