1b5ae7037b7461c0b27096f9f04a7f5f97a9646d
Ксюша <124886145+KsseniyaTerehova@users.noreply.github.com>
2023-04-07 21:08:20 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,37 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
+#include <string>
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+   Type arr[100];
+   int top;
+   
+public:
+    TStack() :top(-1) {}
+    void push(Type value) {
+      if (isFull()) {
+        throw std::string("Full!");
+      } else {
+        arr[++top] = value;
+      }
+    }
+    const Type& pop() {
+      if (isEmpty()) {
+        throw std::string("Empty!");
+      } else {
+        return arr[top--];
+      }
+    }
+    const Type& get()const {
+      return arr[top];
+    }
+    bool isEmpty()const {
+      return top == -1;
+    }
+    bool isFull()const {
+      return top == size - 1;
+    }
 };
-
 #endif  // INCLUDE_TSTACK_H_
