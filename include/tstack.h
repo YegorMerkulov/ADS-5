6c59fe04b74401b2e82d53821f28f8ad9d5c0028
Ксюша <124886145+KsseniyaTerehova@users.noreply.github.com>
2023-04-07 23:29:53 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -16,7 +16,7 @@ class TStack {
       } else {
         arr[++top] = value;
       }
-    }
+    } 
     const Type& pop() {
       if (isEmpty()) {
         throw std::string("Empty!");
@@ -33,6 +33,6 @@ class TStack {
     bool isFull()const {
       return top == size - 1;
     }
-};
+};  
 #endif // INCLUDE_TSTACK_H_
 
