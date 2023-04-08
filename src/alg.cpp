9da7b3ae3274f90e97cd521a81357d84f3d451c9
Ксюша <124886145+KsseniyaTerehova@users.noreply.github.com>
2023-04-08 22:02:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,1 +1,129 @@
+// Copyright 2021 NNTU-CS
+#include <string>
+#include <map>
+#include "tstack.h"
 
+int getPrior(char ch) {
+    std::pair<char, int> prior[6];
+    switch (ch) {
+    case '(':
+        prior[0].first = '(';
+        prior[0].second = 0;
+    case ')':
+        prior[1].first = ')';
+        prior[1].second = 1;
+    case '+':
+        prior[2].first = '+';
+        prior[2].second = 2;
+    case '-':
+        prior[3].first = '-';
+        prior[3].second = 2;
+    case '*':
+        prior[4].first = '*';
+        prior[4].second = 3;
+    case '/':
+        prior[5].first = '/';
+        prior[5].second = 3;
+    }
+    int pr = -1;
+    for (int j = 0; j < 6; ++j) {
+        if (ch == prior[j].first) {
+            pr = prior[j].second;
+            break;
+        }
+    }
+    return pr;
+}
+
+int count1(const int& n1, const int& n2, const int& oper) {
+    switch (oper) {
+        default:
+            break;
+        case '+': return n1 + n2;
+        case '-': return n1 - n2;
+        case '*': return n1 * n2;
+        case '/': return n1 / n2;
+    }
+    return 0;
+}
+
+std::string space(const std::string& s) {
+  if (s.length() <= 2)
+    return s;
+  int n = 2 - s.length() % 2;
+  std::string right(s, 0, n);
+  for (auto it = s.begin() + n; it != s.end();) {
+    right += ' '; right += *it++;;
+  }
+  return right;
+}
+
+std::string infx2pstfx(std::string inf) {
+  // добавьте код
+  std::string pstfx;
+  TStack<char, 100> stChar;
+  for (auto& operation : inf) {
+    int priority = getPrior(operation);
+    if (priority == -1) {
+      pstfx += operation;
+    } else {
+      if (stChar.get() < priority || priority == 0 || stChar.isEmpty()) {
+        stChar.push(operation);
+      } else if (operation == ')') {
+        char ch = stChar.get();
+        while (getPrior(ch) >= priority) {
+          pstfx += ch;
+          stChar.pop();
+          ch = stChar.get();
+        }
+        stChar.pop();
+      } else {
+        char ch = stChar.get();
+        while (getPrior(ch) >= priority) {
+          pstfx += ch;
+          stChar.pop();
+          ch = stChar.get();
+        }
+        stChar.push(operation);
+      }
+    }
+  }
+  while (!stChar.isEmpty()) {
+    pstfx += stChar.get();
+    stChar.pop();
+  }
+  pstfx = space(pstfx);
+  return pstfx;
+}
+  return std::string("");
+}
+
+int eval(std::string pref) {
+  // добавьте код
+  int eval(std::string prf) {
+    TStack <int, 100> stInt;
+    std::string rez = "";
+    for (size_t i = 0; i < prf.size(); i++) {
+        if (getPrior(prf[i]) == -1) {
+            if (prf[i] == ' ') {
+                continue;
+            } else if (isdigit(prf[i + 1])) {
+                rez += prf[i];
+                continue;
+            } else {
+                rez += prf[i];
+                stInt.push(atoi(rez.c_str()));
+                rez = "";
+            }
+        } else {
+          int b = stInt.get();
+          stInt.pop();
+          int a = stInt.get();
+          stInt.pop();
+          stInt.push(count1(a, b, prf[i]));
+        }
+  }
+  return stInt.get();
+}
+  return 0;
+}
